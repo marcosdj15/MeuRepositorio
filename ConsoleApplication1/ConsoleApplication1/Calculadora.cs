5bdcdb103e918b47262a3cf52c00c4249ff46990
marcosdj15 <marcosdj2140@gmail.com>
2016-05-02 21:35:59 -0300

    Classe Soma

---
+++ ConsoleApplication1/ConsoleApplication1/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
